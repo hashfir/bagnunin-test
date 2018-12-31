5795732560c403eabc386824c7949431bf1595ad
hashfir <hashfironaldi@gmail.com>
2018-12-31 18:00:40 +0700

    first

---
+++ src/header.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+class Headernav extends React.Component {
+  render() {
+    const navLogo = {
+      top: "4px"
+    };
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand font-weight-bold text-warning" href="/">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-warning"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Login
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/">
+                  Action
+                </a>
+                <a className="dropdown-item" href="/">
+                  Another action
+                </a>
+                <div className="dropdown-divider" />
+                <a className="dropdown-item" href="/">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <span
+                className="nav-link fa fa-user text-warning"
+                style={navLogo}
+              />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Headernav;
