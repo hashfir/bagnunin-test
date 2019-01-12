c21c8d5d70f9956e75bec59e42c21ea144e988a9
hashfir <hashfironaldi@gmail.com>
2019-01-12 09:55:36 +0700

    bila nnti saat nya tlah tiba

--- src/body/category/category.js
+++ src/body/category/category.js
@@ -19,14 +19,22 @@ class Category extends React.Component {
         <div class="row">
           <div class="col-sm-1 h-100 mr-1 mw-100 border border-warning cardHover important ">
             <div class="card-body">
-              <i class="fa fa-code" />
-              <p class="mb-0">roof</p>
+              <img
+                class="iconLogo mb-2"
+                src="https://firebasestorage.googleapis.com/v0/b/bangunin-98794.appspot.com/o/kategory-paint.png?alt=media&token=fbc34e6d-e637-4983-9e9a-7620747ff26c"
+                alt=" "
+              />
+              <p class="mb-0">Paint</p>
             </div>
           </div>
-          <div class="col-sm-1 h-50 mr-1 mw-100 border border-warning cardHover important ">
-            <div class="card-body ">
-              <i class="fa fa-code" />
-              <p class="mb-0">roof</p>
+          <div class="col-sm-1 h-100 mr-1 mw-100 border border-warning cardHover important ">
+            <div class="card-body">
+              <img
+                class="iconLogo mb-2"
+                src="https://firebasestorage.googleapis.com/v0/b/bangunin-98794.appspot.com/o/tools.png?alt=media&token=398c447f-5ea2-4106-ac81-13afa30374dd"
+                alt=" "
+              />
+              <p class="mb-0">Tools</p>
             </div>
           </div>
           <div class="col-sm-1 h-50 mr-1 mw-100 border border-warning cardHover important ">
