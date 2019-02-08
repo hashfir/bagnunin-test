062e80cc1e1d27f600e8205190a196ded0b6bd02
Pi <hashfir>
2019-02-08 19:34:41 +0700

    tes

--- src/header/nav.js
+++ src/header/nav.js
@@ -7,59 +7,53 @@ class Nav extends React.Component {
       zIndex: 2
     };
     return (
-      <nav
-        class="navbar navbar-expand-sm navbar-dark bg-dark setNavHeight"
-        style={znav}
-      >
-        <div className="container">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link text-warning pl-0" href="/">
+      <nav class="navbar navbar-expand-sm navbar-light bg-dark">
+        <div class="container">
+          <div class="d-flex flex-row">
+            <div class="p-2 nav-item active">
+              <a class="nav-link text-warning mr-0 pr-0 pl-0" href="/">
                 <i class="fas fa-question-circle" />
-                Bantuan | Follow us on <span class="sr-only">(current)</span>
+                Bantuan | Follow us on
               </a>
-            </li>
-            <div class="collapse navbar-collapse">
-              <li class="nav-item ">
-                <a class="nav-link text-warning pl-1 pr-1" href="/">
-                  <i class="fab fa-facebook-f" />
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-warning pl-1" href="/">
-                  <i class="fab fa-instagram" />
-                </a>
-              </li>
             </div>
-          </ul>
+            <div class="p-2 ml-0 nav-item active">
+              <a class="nav-link text-warning pl-0 pr-1" href="/">
+                <i class="fab fa-facebook-f" />
+              </a>
+            </div>
+            <div class="p-2 nav-item active">
+              <a class="nav-link text-warning pl-0" href="/">
+                <i class="fab fa-instagram" />
+              </a>
+            </div>
+          </div>
           <button
             class="navbar-toggler"
             type="button"
             data-toggle="collapse"
-            data-target="#navbarText"
-            aria-controls="navbarText"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon" />
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item bg-dark">
-                <button
-                  type="button"
-                  class="btn btn-sm btn-outline-warning mr-1 mb-"
-                >
-                  Login
-                </button>
-              </li>
-              <li class="nav-item">
-                <button type="button" class="btn btn-sm btn-warning">
-                  register
-                </button>
-              </li>
-            </ul>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="ml-auto nav-item">
+              <div class="d-flex navbar-nav">
+                <div class="p-1 nav-item">
+                  <button type="button" class="btn btn-sm btn-outline-warning">
+                    Login
+                  </button>
+                </div>
+                <div class="p-1 nav-item ">
+                  <button type="button" class="btn btn-sm btn-warning">
+                    register
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
